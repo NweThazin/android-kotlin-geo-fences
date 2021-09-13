9eb58105822c70830f27e71374843b402d3433c7
thazin <nwe.nwethazin@gmail.com>
2021-09-13 23:45:10 +0800

    chore (geogence utils) : open geofence location

--- app/src/main/java/com/example/android/treasureHunt/GeofenceUtils.kt
+++ app/src/main/java/com/example/android/treasureHunt/GeofenceUtils.kt
@@ -54,39 +54,38 @@ internal object GeofencingConstants {
     val GEOFENCE_EXPIRATION_IN_MILLISECONDS: Long = TimeUnit.HOURS.toMillis(1)
 
     val LANDMARK_DATA = arrayOf(
-        LandmarkDataObject(
-            "golden_gate_bridge",
-            R.string.golden_gate_bridge_hint,
-            R.string.golden_gate_bridge_location,
-            LatLng(37.819927, -122.478256)
-        ),
-
-        LandmarkDataObject(
-            "ferry_building",
-            R.string.ferry_building_hint,
-            R.string.ferry_building_location,
-            LatLng(37.795490, -122.394276)
-        ),
-
-        LandmarkDataObject(
-            "pier_39",
-            R.string.pier_39_hint,
-            R.string.pier_39_location,
-            LatLng(37.808674, -122.409821)
-        ),
-
-        LandmarkDataObject(
-            "union_square",
-            R.string.union_square_hint,
-            R.string.union_square_location,
-            LatLng(37.788151, -122.407570)
-        ),
-//,
+//        LandmarkDataObject(
+//            "golden_gate_bridge",
+//            R.string.golden_gate_bridge_hint,
+//            R.string.golden_gate_bridge_location,
+//            LatLng(37.819927, -122.478256)
+//        ),
+//
+//        LandmarkDataObject(
+//            "ferry_building",
+//            R.string.ferry_building_hint,
+//            R.string.ferry_building_location,
+//            LatLng(37.795490, -122.394276)
+//        ),
+//
+//        LandmarkDataObject(
+//            "pier_39",
+//            R.string.pier_39_hint,
+//            R.string.pier_39_location,
+//            LatLng(37.808674, -122.409821)
+//        ),
+//
+//        LandmarkDataObject(
+//            "union_square",
+//            R.string.union_square_hint,
+//            R.string.union_square_location,
+//            LatLng(37.788151, -122.407570)
+//        ),
         LandmarkDataObject(
             "ang_mo_kio",
             R.string.lombard_street_hint,
             R.string.lombard_street_location,
-            LatLng(1.365990628293553, 103.85244781084728)
+            LatLng(1.365990, 103.852447)
         )
     )
 
